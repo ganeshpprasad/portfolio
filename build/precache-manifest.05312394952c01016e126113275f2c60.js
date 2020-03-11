570eac1774e209a5f46f0223004a210691744643
ganeshp <ganeshpprasad@gmail.com>
2020-03-11 19:09:25 +0530

    Initial dist subtree commit

---
+++ build/precache-manifest.05312394952c01016e126113275f2c60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb7b5af688b0321123fb1101ea2c5948",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d34b7b4be8d9ec62be8c",
+    "url": "/portfolio/static/css/main.256f4384.chunk.css"
+  },
+  {
+    "revision": "f6a31973eb87c2a169d2",
+    "url": "/portfolio/static/js/2.5b38881f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.5b38881f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d34b7b4be8d9ec62be8c",
+    "url": "/portfolio/static/js/main.fbd944de.chunk.js"
+  },
+  {
+    "revision": "94c75883374a24d797f5",
+    "url": "/portfolio/static/js/runtime-main.2435769c.js"
+  }
+]);
